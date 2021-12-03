59702b8288e2965d7aad4176bab4c9011c6b3707
Nicholas Payne <payne_nicholas@cat.com>
2021-12-02 18:52:34 -0600

    flake 8

--- src/day2.py
+++ src/day2.py
@@ -23,12 +23,12 @@ def get_data(filepath: str = "./data/day2_sample.txt") -> List[Tuple[str, int]]:
     with open(filepath, "r") as f:
         data = f.readlines()
 
-    return [(l.split(" ")[0], int(l.split(" ")[1])) for l in data]
+    return [(sub.split(" ")[0], int(sub.split(" ")[1])) for sub in data]
 
 
 def calculate_position(data: List[Tuple[str, int]], dim: str = "horizontal") -> int:
 
     _map = _MAP[dim]
-    res = sum([l[1] * _map.get(l[0], 0) for l in data])
+    res = sum([sub[1] * _map.get(sub[0], 0) for sub in data])
 
     return res
