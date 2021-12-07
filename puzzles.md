c2763d8698d848c6fc42b3afe9d2ec7152f42fe0
Nicholas Payne <nic.payne@protonmail.com>
2021-12-06 21:23:44 -0600

    Add smarter solution for day 6 part 2

--- puzzles.md
+++ puzzles.md
@@ -348,4 +348,9 @@ Each day, a 0 becomes a 6 and adds a new 8 to the end of the list, while each ot
 In this example, after 18 days, there are a total of 26 fish. After 80 days, there would be a total of 5934.
 
 Find a way to simulate lanternfish. How many lanternfish would there be after 80 days?
-    7     return counter
+ --- Part Two ---
+Suppose the lanternfish live forever and have unlimited food and space. Would they take over the entire ocean?
+
+After 256 days in the example above, there would be a total of 26984457539 lanternfish!
+
+How many lanternfish would there be after 256 days?   7     return counter
--- src/day6.py
+++ src/day6.py
@@ -18,6 +18,21 @@ def one_day_passes(arr: List[int]) -> List[int]:
     return new_arr
 
 
+def be_smarter(lantern_fish: List[int], num_days: int) -> List[int]:
+
+    # days go from 0 to 8
+    ages = [0] * 9
+    for i in lantern_fish:
+        ages[i] += 1  # track number of fish at each day's age
+
+    for day in range(num_days):
+        today = day % 9
+
+        ages[(today + 7) % 9] += ages[today]
+
+    return ages
+
+
 if __name__ == "__main__":
     data = get_data("./data/day6.txt")
     old_data = [x for x in data]
@@ -25,3 +40,10 @@ if __name__ == "__main__":
         new_data = one_day_passes(old_data)
         old_data = new_data
     print(f"Day 6 solution 1: {len(new_data)}")
+    # lol jk... this'll blow up
+    # for i in range(256 - 80):
+    #     new_data = one_day_passes(old_data)
+    #     old_data = new_data
+    res = be_smarter(data, 256)
+
+    print(f"Day 6 solution 2: {sum(res)}")
