d17e0249311e9ffd2a8586bd0979bfa3bff4b75c
Nicholas Payne <nic.payne@protonmail.com>
2021-12-07 09:56:39 -0600

    Day 7 part 2

--- src/day7.py
+++ src/day7.py
@@ -34,7 +34,37 @@ def find_min(data) -> Tuple[int, int]:
     return min_position, min_fuel
 
 
+def partial_sum(n: int) -> int:
+    """partial_sum.
+
+    calculate sum 1 to n
+
+    Args:
+        n (int): n
+
+    Returns:
+        int:
+    """
+
+    return (n * (n + 1)) // 2
+
+
+def find_harder_min(data: List[int]) -> Tuple[int, int]:
+    min_position = -1
+    min_fuel = sum([partial_sum(abs(0 - j)) for j in data if 0 != j])
+
+    for i in range(max(data)):
+        fuel = sum([partial_sum(abs(i - j)) for j in data if i != j])
+        if fuel < min_fuel:
+            min_fuel = fuel
+            min_position = i
+
+    return min_position, min_fuel
+
+
 if __name__ == "__main__":
     data = get_data("./data/day7.txt")
     pos, fuel = find_min(data)
     print(f"Day 7 part 1 -- position: {pos}, fuel: {fuel}")
+    pos, fuel = find_harder_min(data)
+    print(f"Day 7 part 2 -- position: {pos}, fuel: {fuel}")
--- test/test_day7.py
+++ test/test_day7.py
@@ -1,6 +1,28 @@
 """test day 7"""
 
-from src.day7 import get_data, find_min
+from src.day7 import get_data, find_min, partial_sum, find_harder_min
+
+
+def test_find_harder_min():
+    data = get_data()
+    expected_fuel = 168
+    expected_pos = 5
+
+    pos, fuel = find_harder_min(data)
+
+    assert expected_fuel == fuel
+    assert expected_pos == pos
+
+
+def test_partial_sum():
+    n = 16 - 5
+    expected = 66
+    my_sum = partial_sum(n)
+    assert my_sum == expected
+    n = 5 - 1
+    expected = 10
+    my_sum = partial_sum(n)
+    assert my_sum == expected
 
 
 def test_find_min():
