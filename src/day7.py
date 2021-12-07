b44bdacd20dcb4b74b9aebc0d9f9bd2a27f271a8
Nicholas Payne <nic.payne@protonmail.com>
2021-12-07 09:25:39 -0600

    Add find_min in day 7

--- src/day7.py
+++ src/day7.py
@@ -1,5 +1,5 @@
 """day 7"""
-from typing import List
+from typing import List, Tuple
 
 
 def get_data(filepath: str = "./data/day7_sample.txt") -> List[int]:
@@ -7,3 +7,28 @@ def get_data(filepath: str = "./data/day7_sample.txt") -> List[int]:
     with open(filepath, "r") as f:
         data = f.readline()
     return [int(x) for x in data.split(",")]
+
+
+def find_min(data) -> Tuple[int, int]:
+    """find_min.
+
+    find the value in data such that the sum of distances from all points to a given
+    point is minimized
+
+    Args:
+        data:
+
+    Returns:
+        min_position
+        min_fuel
+    """
+    min_position = -1
+    min_fuel = sum([abs(data[0] - j) for j in data if data[0] != j])
+
+    for i in data[1:]:
+        fuel = sum([abs(i - j) for j in data if i != j])
+        if fuel < min_fuel:
+            min_fuel = fuel
+            min_position = i
+
+    return min_position, min_fuel
--- test/test_day7.py
+++ test/test_day7.py
@@ -1,6 +1,16 @@
 """test day 7"""
 
-from src.day7 import get_data
+from src.day7 import get_data, find_min
+
+
+def test_find_min():
+
+    data = get_data()
+    expected_fuel = 37
+    expected_pos = 2
+    pos, fuel = find_min(data)
+    assert expected_fuel == fuel
+    assert expected_pos == pos
 
 
 def test_get_data():
